5ac8d279199c6f1c2c96ccd02f333c8aaae31bbb
RonaLu <rona26lu@gmail.com>
2020-10-10 22:50:33 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,21 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    tsv_line = tsv.split("\n")
+    keys = tsv_line[0].split("\t")
+    @data = tsv_line[1..-1].map do |line|
+      values = line.chomp.split("\t")
+      keys.zip(values).to_h
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    @data.join("\n") + "\n"
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
+
+require 'yaml'
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  def take_yaml(yaml)
+    yaml_file = YAML.safe_load(yaml)
+    @data = []
+    key_arr = yaml_file[0].keys
+    @data << key_arr.join("\t")
+    yaml_file.each { |line| @data << line.values.join("\t") }
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+end
